126b02fac145f11df2651412923bbc7fcbe38f09
MasterSeafood <410735012@gms.ndhu.edu.tw>
2023-03-29 11:51:14 +0800

    3.29

--- Car_toolkit.py
+++ Car_toolkit.py
@@ -35,31 +35,71 @@ def nextPos(x, y, ang, theta):
 
 def draw_sensors(x, y, angle, walls):
 
-    # Calculate the positions of the three sensors
-    front_sensor = np.array([x, y]) + 50 * np.array([math.cos(math.radians(angle)), math.sin(math.radians(angle))])
-    left_sensor = np.array([x, y]) + 50 * np.array([math.cos(math.radians(angle + 45)), math.sin(math.radians(angle + 45))])
-    right_sensor = np.array([x, y]) + 50 * np.array([math.cos(math.radians(angle - 45)), math.sin(math.radians(angle - 45))])
+    sensor_angles = [angle-45, angle, angle+45]
+    sensors=[]
+    min_ds=[]
+    min_ds_point=[]
+    for sensor_angle in sensor_angles:
+        sensor_end_x = x + 50*math.cos(math.radians(sensor_angle))
+        sensor_end_y = y + 50*math.sin(math.radians(sensor_angle))
+        s_line = [[x, y], [sensor_end_x, sensor_end_y]]
 
-    # Calculate the distances from each sensor to the nearest wall
-    front_distances = np.sqrt(np.sum((walls - front_sensor) ** 2, axis=1))
-    left_distances = np.sqrt(np.sum((walls - left_sensor) ** 2, axis=1))
-    right_distances = np.sqrt(np.sum((walls - right_sensor) ** 2, axis=1))
+        #找出焦點
+        points = []
+        for i in range(len(walls)-1):
+        	w_line = [walls[i], walls[i+1]]
+        	#print(s_line, w_line)
+        	p = line_intersection(s_line, w_line)
+        	if not p==0:
+        		points.append(p)
+        #計算距離
+        distances=[]
+        for p in points:
+        	distances.append(get_distance([x, y], p))
+        ds = np.array(distances)
+        minds = np.min(ds)
+        minidx = np.argmin(ds)
+        	
+        min_ds.append(minds)
+        min_ds_point.append(points[minidx])
+        sensors.append([sensor_end_x, sensor_end_y])
 
-    print(front_sensor, left_sensor, right_sensor)
+    #print("sensors", sensors)
+    #print("min_ds", min_ds)
+    #print("min_ds_point", sensors)
 
-    return front_sensor, left_sensor, right_sensor, front_distances.min(), left_distances.min(), right_distances.min()
+    return sensors, min_ds, min_ds_point
 
+def line_intersection(line1, line2):
+    x1, y1 = line1[0]
+    x2, y2 = line1[1]
+    x3, y3 = line2[0]
+    x4, y4 = line2[1]
 
-def intersection(line1, line2):
-    x1, y1, x2, y2 = line1
-    x3, y3, x4, y4 = line2
-    
-    dx1, dy1 = x2 - x1, y2 - y1
-    dx2, dy2 = x4 - x3, y4 - y3
-    t = (dx2 * (y3 - y1) + dy2 * (x1 - x3)) / denom
-    x, y = x1 + t * dx1, y1 + t * dy1
-    return x, y
+    # 計算線段的向量表示
+    vec1 = [x2 - x1, y2 - y1]
+    vec2 = [x4 - x3, y4 - y3]
+    vec3 = [x3 - x1, y3 - y1]
 
+    # 計算線段的交點
+    denominator = vec1[0] * vec2[1] - vec1[1] * vec2[0]
+    if denominator == 0:
+        return 0  # 無交點
+    t = (vec3[0] * vec2[1] - vec3[1] * vec2[0]) / denominator
+    if t < 0 or t > 1:
+        return 0  # 交點不在線段上
+    u = (vec3[0] * vec1[1] - vec3[1] * vec1[0]) / denominator
+    if u < 0 or u > 1:
+        return 0  # 交點不在線段上
+    p = [x1 + t * vec1[0], y1 + t * vec1[1]]
+    return p
+
+def get_distance(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    dx = x2 - x1
+    dy = y2 - y1
+    return math.sqrt(dx*dx + dy*dy)
 
 def inBox(x, y):
 	return 18 <= x <= 30 and 37 <= y <= 40
--- test.py
+++ test.py
@@ -2,11 +2,12 @@ import Car_toolkit as ct
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle, Rectangle
-
+import matplotlib.animation as animation
+fig, ax = plt.subplots()
 square, car_pos, endline = ct.getSquare("軌道座標點.txt")
-
-while not(ct.inBox(car_pos[0], car_pos[1])):
-	fig, ax = plt.subplots()
+frame=0
+while (not(ct.inBox(car_pos[0], car_pos[1])))and frame<50:
+	
 	plt.xlim([-20, 50])
 	plt.ylim([-10, 60])
 	sX = square[:, 0]; sY = square[:, 1]
@@ -14,19 +15,53 @@ while not(ct.inBox(car_pos[0], car_pos[1])):
 	rect = Rectangle((18, 37), 12, 3, linewidth=1, edgecolor='r', facecolor='none')
 	circle = Circle((car_pos[0], car_pos[1]), 3, fill=False)
 
-	fs, ls, rs, fd, ld, rd = ct.draw_sensors(car_pos[0], car_pos[1], car_pos[2], square)
+	sensors, min_ds, min_ds_point = ct.draw_sensors(car_pos[0], car_pos[1], car_pos[2], square)
+	rs, fs, ls = sensors
+	rd, fd, ld = min_ds
+	rp, fp, lp = min_ds_point
+	
+	theta=0
+	if rd<10:
+		theta=0
+	elif fd<10:
+		theta=40
+	elif ld<10:
+		theta=0
 
 	ax.plot([car_pos[0], fs[0]], [car_pos[1], fs[1]], 'r-', linewidth=2)
 	ax.plot([car_pos[0], ls[0]], [car_pos[1], ls[1]], 'g-', linewidth=2)
 	ax.plot([car_pos[0], rs[0]], [car_pos[1], rs[1]], 'b-', linewidth=2)
 
+	for i in range(3):
+		sp = np.array(min_ds_point[i])
+		plt.scatter(sp[0], sp[1])
+		plt.annotate(str(round(min_ds[i], 2)), xy=(sp[0], sp[1]), xytext=(sp[0]+0.1, sp[1]+0.1))
+
 	ax.add_artist(circle)
 	ax.add_artist(rect)
-	plt.show()
+	car_pos = ct.nextPos(car_pos[0], car_pos[1], car_pos[2], theta)
+	
+	file_name="./index/"+str(frame)+".png"
+	fig.savefig(file_name)
+	print(file_name, "saved")
+	ax.cla()
+	frame += 1
 
+from PIL import Image
+import os
 
-	car_pos = ct.nextPos(car_pos[0], car_pos[1], car_pos[2], 0)
-	#print("fd", fd)
-	#print("ld", ld)
-	#print("rd", rd)
+print("saving gif")
+folder_path = "./index"
+image_list = []
+for filename in os.listdir(folder_path):
+    if filename.endswith(".png"):
+        number = int(filename.split(".")[0])
+        image_path = os.path.join(folder_path, filename)
+        image = Image.open(image_path)
+        image_list.append((number, image))
 
+image_list.sort(key=lambda x: x[0])
+sorted_images = [img for _, img in image_list]
+
+save_path = "./animation.gif"
+sorted_images[0].save(save_path, save_all=True, append_images=sorted_images[1:], duration=100, loop=0)
